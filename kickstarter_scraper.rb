cb5d7d8ddc566967f3e79b18ed18206947b8c5c5
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-23 00:27:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,7 +2,9 @@ require 'nokogiri'
 require 'pry'
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
-# project.css("div.project-thumbnail a img").attribute("src").value
+# image: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
