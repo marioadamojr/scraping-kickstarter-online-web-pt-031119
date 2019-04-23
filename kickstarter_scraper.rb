67245fca22ba350f4cf7856728a59c574c989cc0
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-23 00:43:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,10 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+
+  projects = {}
+
+  # binding.pry
 end
 
 create_project_hash
