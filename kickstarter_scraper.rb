962076e3996836fbecb8428a64e1c824b4c62be1
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-23 00:43:16 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 # image: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
-# percent funded: project.css("ul.project-stats li.first.funded strong").text
+# percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 
 def create_project_hash
