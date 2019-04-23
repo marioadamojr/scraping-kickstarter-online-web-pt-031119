c8e47b3eb43557ec65e26a9dd2d92d008e588fba
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-23 00:45:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,9 @@ def create_project_hash
 
   projects = {}
 
-  kickstarter.css("li.project.grid_4")
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
 
   # binding.pry
 end
